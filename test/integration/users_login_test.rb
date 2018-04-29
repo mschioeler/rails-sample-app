362d3b6f933c280a642fd77c9cc7ab248b3da8d0
Morten Emmanuel Schiøler <mschioeler@deloitte.dk>
2018-04-30 00:58:58 +0200

    Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,6 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email: @user.email, password: "password" }}
     assert_redirected_to @user
+    assert tv_logged_in?
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
@@ -31,7 +32,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
